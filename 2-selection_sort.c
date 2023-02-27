8688b05af809638c59ac6b3f48d3f0bcc2afba92
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-27 04:59:21 +0100

    selection sort final fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,19 +24,21 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
 
-	if (size < 2 || !array)
+	if (size < 2 || array == NULL)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
-		for (j = i + 1; j < size; j++)
+		for (j = i; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
 		}
 		if (min_idx != i)
+		{
 			swap(&array[i], &array[min_idx]);
-		print_array(array, size);
+			print_array(array, size);
+		}
 	}
 }
