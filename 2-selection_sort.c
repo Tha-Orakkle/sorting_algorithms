5eceac1b57a2aaacebee042dbe1bf23ea2a89ae5
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-27 04:50:33 +0100

    selection sort fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 	if (size < 2 || !array)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
