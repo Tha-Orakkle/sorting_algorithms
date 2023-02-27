a8bc40e139b28ab00d4850c572d2f528f7b27f8f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-02-27 03:49:04 +0100

    insertion sorting algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,5 +6,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	/* pass */
+	listint_t *iter, *temp, *last;
+
+	if (list == NULL || (*list) == NULL || (*list)->next == NULL)
+		return;
+
+	iter = (*list)->next;
+
+
+	while (iter != NULL)
+	{
+		temp = iter->next;
+		last = iter->prev;
+		while (last && last->n > iter->n)
+		{
+			last->next = iter->next;
+			if (iter->next)
+				iter->next->prev = last;
+			iter->prev = last->prev;
+			iter->next = last;
+			if (last->prev)
+				last->prev->next = iter;
+			else
+				(*list) = iter;
+			last->prev = iter;
+			last = iter->prev;
+
+			print_list(*list);
+		}
+		iter = temp;
+	}
 }
